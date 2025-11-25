706ca877c83420345cbb9416c4ea29cdb6f37902
SeolWolWha <leeyoon97@naver.com>
2025-11-25 19:15:17 +0900

    Style: 검색 결과 페이지 퍼블리싱

--- src/app/search/[searchid]/page.tsx
+++ src/app/search/[searchid]/page.tsx
@@ -1,7 +1,9 @@
+import ResultPage from "@/components/Search/ResultPage";
+
 export default function page () {
   return (
     <>
-      <h1>page Component</h1>
+      <ResultPage/>
     </>
   );
 }
--- src/components/Category/CategoryPage.tsx
+++ src/components/Category/CategoryPage.tsx
@@ -7,7 +7,7 @@ export default function CategoryPage() {
   const location = usePathname();
   return (
     <>
-      <div className="flex flex-col pt-[25px]">
+      <div className="flex flex-col pt-[25px] px-[10px] w-full">
         {location === "/category" ? <CategoryGroup /> : <CategoryDetail />}
       </div>
     </>
