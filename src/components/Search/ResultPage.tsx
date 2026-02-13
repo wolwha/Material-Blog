7ff52886a5e111ebb3a392dbcaa72b70b528aaa2
WolWha <leeyoon97@naver.com>
2026-02-13 19:05:38 +0900

    feat: 검색 결과 DB 동기화

--- src/components/Search/ResultPage.tsx
+++ src/components/Search/ResultPage.tsx
@@ -1,26 +1,51 @@
 'use client';
 
+import { getSearchData } from '@/services/searchService';
 import { useSearchStore } from '@/stores/searchStore';
+import { SearchType } from '@/types/searchType';
 import { useParams, useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import { MdOutlineClose, MdSearch } from 'react-icons/md';
+import PostCard from '../Main/PostCard';
 
 export default function ResultPage() {
   const params = useParams();
-  console.log(decodeURIComponent(params.searchid as string));
   const [value, setValue] = useState<string>('');
+  const [result, setResult] = useState<SearchType | null>(null);
   const { setParam, param } = useSearchStore();
   const router = useRouter();
+  const rawKeyword = params.searchid;
+  const keyword =
+    typeof rawKeyword === 'string' ? decodeURIComponent(rawKeyword) : '';
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     if (!value.trim()) return;
+    console.log(value);
     setParam(value);
     router.push(`/search/${encodeURIComponent(value)}`);
     setValue('');
   };
   useEffect(() => {
-    setValue(param);
+    if (param) {
+      setValue(param);
+    } else if (keyword) {
+      setValue(keyword);
+    }
   }, [param]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (!keyword) return;
+      try {
+        const data = await getSearchData(keyword);
+        setResult(data);
+        console.log(data);
+      } catch (error) {
+        console.error('데이터 요청 실패: ', error);
+      }
+    };
+    fetchData();
+  }, [keyword]);
 
   return (
     <>
@@ -34,17 +59,17 @@ export default function ResultPage() {
               onChange={(e) => setValue(e.target.value)}
               value={value}
             />
-            {value === '' ? (
+            <button
+              className="absolute right-5 flex size-12.5 cursor-pointer text-center text-[70px]"
+              type="submit"
+            >
+              <MdSearch />
+            </button>
+            {value === '' ? null : (
               <button
-                className="absolute right-5 flex size-12.5 cursor-pointer text-center text-[70px]"
-                type="submit"
-              >
-                <MdSearch />
-              </button>
-            ) : (
-              <button
-                className="absolute right-5 flex size-12.5 cursor-pointer text-center text-[70px]"
+                className="absolute top-3.5 right-18 flex size-12.5 cursor-pointer text-center text-[40px]"
                 onClick={() => setValue('')}
+                type="button"
               >
                 <MdOutlineClose />
               </button>
@@ -53,49 +78,98 @@ export default function ResultPage() {
         </div>
         <div className="flex w-full flex-col items-center justify-center">
           <div className="flex w-full justify-start px-4.5 py-10">
-            <p className="text-[32px] font-bold">
-              {decodeURIComponent(params.searchid as string)}의 검색 결과
-            </p>
+            <p className="text-[32px] font-bold">{keyword}의 검색 결과</p>
           </div>
-          <div className="relative mx-6.25 flex w-full border-b px-5 text-start">
-            <p className="text-[24px] font-semibold">태그</p>
-            <button
-              className="absolute right-5 bottom-0 cursor-pointer hover:underline"
-              name="더보기 버튼"
-              aria-label="더보기 버튼"
-            >
-              더보기
-            </button>
-          </div>
-          <div className="mx-6.25 mt-3 mb-6.25 grid grid-rows-2 gap-x-3.75 gap-y-5 sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
-            {/* 기능개발시 grid rows는 2줄까지 설정 */}
-          </div>
-          <div className="relative mx-6.25 flex w-full border-b px-5 text-start">
-            <p className="text-[24px] font-semibold">카테고리</p>
-            <button
-              className="absolute right-5 bottom-0 cursor-pointer hover:underline"
-              name="더보기 버튼"
-              aria-label="더보기 버튼"
-            >
-              더보기
-            </button>
-          </div>
-          <div className="mx-6.25 mt-3 mb-6.25 grid grid-rows-2 gap-x-3.75 gap-y-5 sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
-            {/* 기능개발시 grid rows는 2줄까지 설정 */}
-          </div>
-          <div className="relative mx-6.25 flex w-full border-b px-5 text-start">
-            <p className="text-[24px] font-semibold">제목</p>
-            <button
-              className="absolute right-5 bottom-0 cursor-pointer hover:underline"
-              name="더보기 버튼"
-              aria-label="더보기 버튼"
-            >
-              더보기
-            </button>
-          </div>
-          <div className="mx-6.25 mt-3 mb-6.25 grid grid-rows-2 gap-x-3.75 gap-y-5 sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
-            {/* 기능개발시 grid rows는 2줄까지 설정 */}
-          </div>
+          {result?.toTag.length !== 0 ? (
+            <>
+              <div className="relative mx-6.25 flex w-full border-b px-5 text-start">
+                <p className="text-[24px] font-semibold">태그</p>
+                <button
+                  className="absolute right-5 bottom-0 cursor-pointer hover:underline"
+                  name="더보기 버튼"
+                  aria-label="더보기 버튼"
+                >
+                  더보기
+                </button>
+              </div>
+              <div className="mx-6.25 mt-3 mb-6.25 grid w-full grid-rows-2 gap-x-3.75 gap-y-5 sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
+                {/* 기능개발시 grid rows는 2줄까지 설정 */}
+                {result?.toTag.slice(0, 8).map((post) => (
+                  <PostCard
+                    Title={post.Title}
+                    Category={post.Category}
+                    Tags={post.Tags}
+                    created_at={post.created_at}
+                    id={post.id}
+                    Content={post.Content}
+                    Context={post.Context}
+                    Thumbnail={post.Thumbnail}
+                    key={post.id}
+                  />
+                ))}
+              </div>
+            </>
+          ) : null}
+          {result?.toCategory.length !== 0 ? (
+            <>
+              <div className="relative mx-6.25 flex w-full border-b px-5 text-start">
+                <p className="text-[24px] font-semibold">카테고리</p>
+                <button
+                  className="absolute right-5 bottom-0 cursor-pointer hover:underline"
+                  name="더보기 버튼"
+                  aria-label="더보기 버튼"
+                >
+                  더보기
+                </button>
+              </div>
+              <div className="mx-6.25 mt-3 mb-6.25 grid w-full grid-rows-2 gap-x-3.75 gap-y-5 sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
+                {/* 기능개발시 grid rows는 2줄까지 설정 */}
+                {result?.toCategory.slice(0, 8).map((post) => (
+                  <PostCard
+                    Title={post.Title}
+                    Category={post.Category}
+                    Tags={post.Tags}
+                    created_at={post.created_at}
+                    id={post.id}
+                    Content={post.Content}
+                    Context={post.Context}
+                    Thumbnail={post.Thumbnail}
+                    key={post.id}
+                  />
+                ))}
+              </div>
+            </>
+          ) : null}
+          {result?.toTitle.length !== 0 ? (
+            <>
+              <div className="relative mx-6.25 flex w-full border-b px-5 text-start">
+                <p className="text-[24px] font-semibold">제목</p>
+                <button
+                  className="absolute right-5 bottom-0 cursor-pointer hover:underline"
+                  name="더보기 버튼"
+                  aria-label="더보기 버튼"
+                >
+                  더보기
+                </button>
+              </div>
+              <div className="mx-6.25 mt-3 mb-6.25 grid w-full gap-x-3.75 gap-y-5 sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
+                {/* 기능개발시 grid rows는 2줄까지 설정 */}
+                {result?.toTitle.slice(0, 8).map((post) => (
+                  <PostCard
+                    Title={post.Title}
+                    Category={post.Category}
+                    Tags={post.Tags}
+                    created_at={post.created_at}
+                    id={post.id}
+                    Content={post.Content}
+                    Context={post.Context}
+                    Thumbnail={post.Thumbnail}
+                    key={post.id}
+                  />
+                ))}
+              </div>
+            </>
+          ) : null}
         </div>
       </div>
     </>
